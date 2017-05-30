71e420a7aba7a667e1ece45ec328612c15d92a87
Kyriacos Kyriacou <kyriacos.s.kyriacou@gmail.com>
2017-05-30 12:32:24 +0300

    coverall

--- application.go
+++ application.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Beanstalk from Codeship!! %v", time.Now())
+		num, _ := strconv.Atoi(r.FormValue("num"))
+		fmt.Fprintf(w, "Hello, %d is %s", num, size(num))
 	})
+
 	log.Println("Listening on port 5000...")
 	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func size(a int) string {
+	switch {
+	case a < 0:
+		return "negative"
+	case a == 0:
+		return "zero"
+	case a < 10:
+		return "small"
+	case a < 100:
+		return "big"
+	case a < 1000:
+		return "huge"
+	}
+	return "enormous"
 }
