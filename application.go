724cb462aa3d9f7be0f0593e21b495238a619cbb
Kyriacos Kyriacou <kyriacos.s.kyriacou@gmail.com>
2017-05-30 11:38:35 +0300

    trigger codeship

--- application.go
+++ application.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello AWS Beanstalk from Codeship! %v", time.Now())
+		fmt.Fprintf(w, "Hello Beanstalk from Codeship! %v", time.Now())
 	})
 	log.Println("Listening on port 5000...")
 	log.Fatal(http.ListenAndServe(":5000", nil))
